e60e49a5513894b8622f68afbd8325a7913232cb
NOLedCorp <noledcorp@gmail.com>
2020-10-09 12:44:27 +0300

    #main-menu redesign

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,6 +34,7 @@ export class HomeComponent implements OnInit {
 
   toCar(id){
     this.router.navigate(['/cars', id]);
+    sessionStorage.setItem('cars', JSON.stringify(this.hightCars.concat(this.lowCars)));
   }
 
   public ngbDateToString(date: NgbDate): string {
