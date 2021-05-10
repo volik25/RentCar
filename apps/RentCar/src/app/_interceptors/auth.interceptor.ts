18494f395b059d85fa82896d15a849527ed748aa
iv@zaoblako.com <i.a.volik@gmail.com>
2021-05-10 21:29:10 +0300

    structure updated

--- apps/RentCar/src/app/_interceptors/auth.interceptor.ts
+++ apps/RentCar/src/app/_interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoadingService } from '../services/loading.service';
+import { LoadingService } from '../_services/loading.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -9,8 +9,8 @@ export class AuthInterceptor implements HttpInterceptor {
 
   public intercept(req: HttpRequest<{}>, next: HttpHandler): Observable<HttpEvent<{}>> {
     let params = req;
-    if (sessionStorage.getItem('bookingUserToken')) {
-      let token = sessionStorage.getItem('bookingUserToken');
+    if (sessionStorage.getItem('rentCar.user.token')) {
+      let token = sessionStorage.getItem('rentCar.user.token');
       const paramReq = req.clone({
         params: req.params.set('token', token),
       });
